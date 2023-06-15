018bfdac6554a34c1e6dff83b0253616c71d3af8
Ogabek <khamdamov343@gmail.com>
2023-06-15 18:07:24 +0500

    changes

--- src/ProjectForInterview.java
+++ src/ProjectForInterview.java
@@ -4,7 +4,6 @@ public class ProjectForInterview {
     private static final MainService mainService = MainService.getInstance();
 
     public static void main(String[] args){
-        System.out.println("Program started");
         mainService.start();
     }
 }
